bd7e1f9663ff153ecba43cadab1d6c41c2d91c27
Anes Hodza <anes.hodza@renuo.ch>
2023-06-16 16:04:07 +0200

    Add file parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::fs::File;
+use std::fs::{File, read_to_string};
 use std::path::{Path, PathBuf};
 use std::io;
 use std::process::exit;
 
+use chrono::{NaiveDate, Duration, Local};
+
 fn main() {
     let file_dir = dirs::home_dir().unwrap().join(".birthdays.txt");
     if check_for_note_file(&file_dir) {
-        println!("File exists");
+        print_birthdays(&file_dir);
     } else {
         initialize_note_file(&file_dir);
     }
@@ -16,6 +18,31 @@ fn check_for_note_file(dir: &PathBuf) -> bool {
     return Path::new(dir).exists();
 }
 
+fn print_birthdays(dir: &PathBuf) {
+    let contents = read_to_string(&dir).expect("Unable to read file");
+    if contents.is_empty() {
+        println!("You dont have any birthdays yet!");
+        exit(0);
+    }
+    let mut lines: Vec<&str> = contents.split("\n").collect();
+    lines = lines[..lines.len() - 1].to_vec();
+    search_soon_birthday(lines);
+}
+
+fn search_soon_birthday(data: Vec<&str>) {
+    let today = Local::now();
+    for row in data.iter() {
+        parse_row(row, today);
+    }
+}
+
+fn parse_row(row: &str, today: chrono::DateTime<Local>) {
+    let split_row: Vec<&str> = row.split("=").collect();
+    let name = split_row[0];
+    let date_of_birth = NaiveDate::parse_from_str(split_row[1], "%Y.%m.%d").expect("Invalid date format");
+    println!("{} has birthday on the {:?}", name, date_of_birth);
+}
+
 fn initialize_note_file(dir: &PathBuf) {
     println!("Looks like you didn't initialize your birthday init file yet.");
     println!("Would you like to initialize it now? (y/n)");
@@ -23,6 +50,7 @@ fn initialize_note_file(dir: &PathBuf) {
         println!("Initializing file...");
         File::create(dir).expect("Unable to create file");
         println!("File initialized!");
+        main();
     } else {
         println!("Alright. Bye!");
         exit(69);
